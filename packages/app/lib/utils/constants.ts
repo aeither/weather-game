7a995cb6e0473d39a920422f955a87e30075b4eb
aeither <36173828+aeither@users.noreply.github.com>
2023-03-21 16:12:08 +0100

    add types

--- packages/app/lib/utils/constants.ts
+++ packages/app/lib/utils/constants.ts
@@ -1,4 +1,9 @@
-import { GaslessOnboarding } from '@gelatonetwork/gasless-onboarding'
+import {
+  GaslessOnboarding,
+  GaslessWalletConfig,
+  GaslessWalletInterface,
+  LoginConfig,
+} from '@gelatonetwork/gasless-onboarding'
 
 if (!process.env.NEXT_PUBLIC_RPC) throw new Error('NEXT_PUBLIC_RPC not found')
 const NEXT_PUBLIC_RPC = process.env.NEXT_PUBLIC_RPC
@@ -9,24 +14,24 @@ const NEXT_PUBLIC_1BALANCE_API_KEY = process.env.NEXT_PUBLIC_1BALANCE_API_KEY
 
 export const CONTRACT_ADDRESS = ''
 
-export const gaslessWalletConfig = {
-  apiKey: NEXT_PUBLIC_1BALANCE_API_KEY,
-}
-
 let origin
 if (typeof window !== 'undefined') {
   origin = window.location.origin
 }
 
-export const loginConfig = {
+export const loginConfig: LoginConfig = {
   domains: [origin],
   chain: {
-    id: 5,
+    id: 5, // eth-goerli
     rpcUrl: NEXT_PUBLIC_RPC,
   },
   openLogin: {
     redirectUrl: origin,
   },
+}
+
+export const gaslessWalletConfig: GaslessWalletConfig = {
+  apiKey: NEXT_PUBLIC_1BALANCE_API_KEY,
 }
 
 export const gaslessOnboarding = new GaslessOnboarding(loginConfig, gaslessWalletConfig)
